16cd6dbd4bb4ce19cd79cc0f1e329011bb5db55a
Alansito-kun <al.carov@duocuc.cl>
2025-05-14 00:12:10 -0400

    generacion de tabla cliente

---
+++ parcial2/evaluacion2/src/main/java/com/model/cliente.java
@@ -0,0 +1,37 @@
+package com.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+/* cliente necesita
+ * - rut
+ * - nombre
+ * - apellido
+ * - telefono
+ * - email
+ */
+@Entity
+@Table(name = "clientes")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+
+public class Cliente {
+    @Id
+    private String rut;
+
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String email;
+    private String direccion;
+
+    @Column(name = "fecha_nacimiento")
+    /* fechas dia/mes/año */
+    private String fechaNacimiento;
+
+    private String sexo;
+    private String password;
+}
